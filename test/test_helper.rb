e38583ddc95ba7c49161f60ed97f28b985f56507
Jan Schill <schill@hey.com>
2021-09-21 13:46:43 +0200

    Fix lint offenses
    Co-authored-by: Chaymae Bazzaoui <bazzaoui.chaymae@gmail.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,54 +1,58 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require 'irb'
+require "irb"
 require "ulid/rails"
 
 require "minitest/autorun"
 
 db_sets = {
   "sqlite3" => {
-    adapter: 'sqlite3',
-    name: ':memory:'
+    adapter: "sqlite3",
+    name: ":memory:"
   },
   "mysql56" => {
-    host: 'mysql56',
-    adapter: 'mysql2',
-    username: 'root',
-    password: 'password',
-    name: 'test'
+    host: "mysql56",
+    adapter: "mysql2",
+    username: "root",
+    password: "password",
+    name: "test"
   },
   "mysql57" => {
-    host: 'mysql57',
-    adapter: 'mysql2',
-    username: 'root',
-    password: 'password',
-    name: 'test'
+    host: "mysql57",
+    adapter: "mysql2",
+    username: "root",
+    password: "password",
+    name: "test"
   },
   "mysql80" => {
-    host: 'mysql80',
-    adapter: 'mysql2',
-    username: 'root',
-    password: 'password',
-    name: 'test'
+    host: "mysql80",
+    adapter: "mysql2",
+    username: "root",
+    password: "password",
+    name: "test"
   },
   "pg12" => {
-    host: 'pg12',
-    adapter: 'postgresql',
-    username: 'postgres',
-    name: 'test'
-  },
+    host: "pg12",
+    adapter: "postgresql",
+    username: "postgres",
+    name: "test"
+  }
 }
-db = db_sets[ENV["DB"]] || db_sets['sqlite3']
+db = db_sets[ENV["DB"]] || db_sets["sqlite3"]
 
-if db[:adapter] != 'sqlite3'
+if db[:adapter] != "sqlite3"
   ActiveRecord::Base.establish_connection(
     host: db[:host],
     adapter: db[:adapter],
     username: db[:username],
-    password: db[:password],
-  #  database: db[:name]
+    password: db[:password]
+    #  database: db[:name]
   )
 
-  ActiveRecord::Base.connection.drop_database(db[:name]) rescue nil
+  begin
+    ActiveRecord::Base.connection.drop_database(db[:name])
+  rescue
+    nil
+  end
   ActiveRecord::Base.connection.create_database(db[:name])
 
   ActiveRecord::Base.connection.disconnect!
